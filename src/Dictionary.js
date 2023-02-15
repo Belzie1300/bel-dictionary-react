5d77b65507cba9754b8069b4d19909ce9de07050
Belzie <dinh.belinda@gmail.com>
2023-02-15 16:26:02 +1100

    integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyboard] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Just a moment, searching for "${keyword}"`);
+
+    let apiKey = "34b7a5f9e78b04eob5f3acad597t59d2";
+    let apiURL = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiURL).then(handleResponse);
   }
   function loadKeyword(event) {
     setKeyboard(event.target.value);
@@ -16,7 +24,7 @@ export default function Dictionary() {
       <div>What word do you want to look up?</div>
       <form onSubmit={handleSubmit}>
         <input type="search" onChange={loadKeyword}></input>
-        <input type="submit"></input>
+        <input type="submit" value="Enter"></input>
       </form>
     </div>
   );
